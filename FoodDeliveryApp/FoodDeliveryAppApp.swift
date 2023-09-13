a84f561673907151f909ba756a955d44468b6afa
Avinash K <kumaravi651@gmail.com>
2023-09-13 19:48:46 +0530

    Initial Commit

---
+++ FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Avinash Kumar on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
